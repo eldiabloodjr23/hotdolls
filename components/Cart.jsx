36f4abdd6cb12fec08ebb884d3244a960feef3b1
eldiablood23 <moneyemoji.pro@gmai.com>
2022-07-26 19:42:42 +0200

    my changes

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -95,7 +95,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Součet:</h3>
-              <h3>${totalPrice}</h3>
+              <h3>CZK{totalPrice}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
