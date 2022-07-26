90f72b3ad8615996b037bf50bcaca983c03ab2ca
eldiablood23 <moneyemoji.pro@gmai.com>
2022-07-26 20:17:29 +0200

    First commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JIRI BEJCEK STUDIO</title>
+        <title>Hotdolls </title>
       </Head>
       <header>
         <Navbar />
