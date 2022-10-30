6f1fa60cf9f29b0494fb78e062077ebf84e69eb6
eldiablood23 <moneyemoji.pro@gmai.com>
2022-10-30 19:36:51 +0100

    first commit

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
 
           return {
             price_data: { 
-              currency: 'CZK',
+              currency: 'czk',
               product_data: { 
                 name: item.name,
                 images: [newImage],
